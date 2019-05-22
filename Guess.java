4b9d2147b6a179b233071db84c5be41ce46d28c9
PaelentheAwesom <dawnplaya55@gmail.com>
2019-05-22 15:04:37 -0700

    more work on Guess

--- Guess.java
+++ Guess.java
@@ -1,3 +1,4 @@
+//Andrew Y
 import pkg.*;
 import java.util.ArrayList;
 
@@ -7,12 +8,21 @@ public class Guess extends Word {
 	String word = Word.getRandomWord();
 
 	public Guess() {
+		makeBoxes();
 		for (int i = 0; i < word.length(); i++) {
-			if(word.substring(i, i + 1)) {
 
-			}
-			boxes.add();
 			
+			
+		}
+	}
+	private void makeBoxes(){
+		for (int i = 0; i < word.length(); i++) {
+			if(!word.substring(i, i + 1).equals(" ")) {
+				boxes.add(new Text(100 + i*10, 200, " "));
+			} else if(!word.substring(i, i + 1).equals("-")) {
+				word.substring(i, i + 1).equals("-")
+			} else 
+				boxes.add(new Text(100 + i*10, 200, "_"));
 		}
 	}
 }
