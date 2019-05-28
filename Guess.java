64b22179276bc82268dff5ed6e4ab0f4f7082298
PaelentheAwesom <dawnplaya55@gmail.com>
2019-05-28 14:51:09 -0700

    made a makeGuess method, will add buttons and stuff

--- Guess.java
+++ Guess.java
@@ -1,28 +1,31 @@
 //Andrew Y
 import pkg.*;
+import pkg.Button;
 import java.util.ArrayList;
 
 public class Guess extends Word {
 
-	ArrayList<Text> boxes = new ArrayList<Text>(); 
-	String word = Word.getRandomWord();
+	private ArrayList<Text> boxes = new ArrayList<Text>(); 
+	private String word = Word.getRandomWord();
+	private char letters[] = word.getCharArray();
+	private String alphabet[] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
 
 	public Guess() {
 		makeBoxes();
-		for (int i = 0; i < word.length(); i++) {
-
-			
-			
-		}
 	}
+
 	private void makeBoxes(){
 		for (int i = 0; i < word.length(); i++) {
 			if(!word.substring(i, i + 1).equals(" ")) {
 				boxes.add(new Text(100 + i*10, 200, " "));
 			} else if(!word.substring(i, i + 1).equals("-")) {
-				word.substring(i, i + 1).equals("-")
+				word.substring(i, i + 1).equals("-");
 			} else 
 				boxes.add(new Text(100 + i*10, 200, "_"));
 		}
+	}
+
+	public boolean doGuess(char g) {
+		for (char x: letters) if (g.equals("x")) return true;
 	}
 }
