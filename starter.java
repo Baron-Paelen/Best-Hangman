3fb89fe1c541c056921a5b30bc7eeee471dc3280
Underslash12 <stew_smith@hotmail.com>
2019-05-30 14:47:36 -0700

    finished starter
    cc

--- starter.java
+++ starter.java
@@ -5,7 +5,7 @@ public class starter {
 	
 	public static void main(String... args)
 	{
-		GameState s = new Player();
+		GameState s = new Hangman(Word.getRandomWord());
 		GameDriver d = new GameDriver(s);
 		d.play();
 		System.out.println(s.getWinner());
