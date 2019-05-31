f24b3eb0436e0b2d83493ab73f74d05c95e86b27
Abyss21402 <coledcaneva@gmail.com>
2019-05-31 14:16:06 -0700

    Update Guess.java

--- Guess.java
+++ Guess.java
@@ -9,7 +9,6 @@ public class Guess extends Word {
 	private String word = Word.getRandomWord();
 	private char letters[] = word.getCharArray();
 	private String alphabet[] = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
-
 	public Guess() {
 		makeBoxes();
 	}
